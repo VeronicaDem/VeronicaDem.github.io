f30cca44839f542296a30ecf6929af6609008b25
VeronicaDem <51059355+VeronicaDem@users.noreply.github.com>
2019-11-09 20:24:33 +0300

    Update script.js

--- 7/script.js
+++ 7/script.js
@@ -80,6 +80,13 @@ $(document).ready(function() {
                         slidesToShow:2,
                         slidesToScroll:2
                     }
+                },
+                {
+                    breakpoint:768,
+                    setting: {
+                        slidesToShow:2,
+                        slidesToScroll:2
+                    }
                 }
             ]
         })
