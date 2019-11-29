86b0955863def94fabac2a1803291bb46a54784e
VeronicaDem <51059355+VeronicaDem@users.noreply.github.com>
2019-11-29 11:39:00 +0300

    Update script.js

--- 8/script.js
+++ 8/script.js
@@ -1,3 +1,4 @@
+let current = "index";
 $(".hamburger").click(function(ev) {
     $(".mobile-header-list").toggle();
 })
@@ -7,9 +8,17 @@ $(".btn-menu").click(function() {
 $(".exit").click(function() {
     $(".popup-container").hide();
     document.body.style.overflow = "auto";
+    current = "index";
+    history.back();
 })
 $(".btn-call").click(()=>{
     $(".popup-container").show();
+    curren = "contact";
     document.body.style.overflow = "hidden";
     history.pushState(null, null, "./contact");
 })
+window.onpopstate((e)=> {
+  if(current == "index") {
+     $(".popup-container").hide();
+  }
+});
