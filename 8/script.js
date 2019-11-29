7dfb6b0b64d6bb2b282609b62005c321e7729f08
VeronicaDem <51059355+VeronicaDem@users.noreply.github.com>
2019-11-29 12:49:52 +0300

    Update script.js

--- 8/script.js
+++ 8/script.js
@@ -1,4 +1,36 @@
 let current = "index";
+let current_fio = window.localStorage.getItem('fio');
+let current_telephone = window.localStorage.getItem('telephone');
+let current_region = window.localStorage.getItem('region');
+let current_message = window.localStorage.getItem('message');
+if(current_fio) {
+   $("[name='fio']").val(current_fio);
+}
+if(current_telephone) {
+  $('[name="telephone"]').val(current_telephone);
+}
+if(current_region) {
+   $(`[value='${current_region}'`)[0].selected = true;
+}
+if(current_message) {
+   $("[name='textarea']").val(current_message);
+}
+$("[name='fio']").bind('input',(e)=> {
+  current_fio = e.target.value;
+  window.localStorage.setItem('fio',current_fio);
+})
+$("[name='telephone']").bind('input',(e)=> {
+  current_telephone =e.target.value;
+  window.localStorage.setItem('telephone',current_telephone);
+})
+$('select').on('change',(e)=>{
+  current_region = this.value;
+  window.localStorage.setItem('region',current_region);
+})
+$('textarea').bind('input', (e)=> {
+  current_message = this.value;
+  window.localStorage.setItem('message',current_message);
+})
 $(".hamburger").click(function(ev) {
     $(".mobile-header-list").toggle();
 })
